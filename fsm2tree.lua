f37ddf31589baab2cb137a7f645438bd26bf8b03
Markus Klotzbuecher <mk@marumbi.de>
2009-10-23 17:39:47 +0200

    Stub for FSM tree visualization

---
+++ fsm2tree.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/lua
+
+require("gv")
+
+local pairs, ipairs, print, table, type, assert, gv, io
+   = pairs, ipairs, print, table, type, assert, gv, io
+
+module("fsm2tree")
+
+
+
+
+
+
+
+
+function fsm2tree(fsm, outfile)
+
+   
+
+end
