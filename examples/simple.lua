09c2d2e92510ce28c407e37025d5df889fd83bf7
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2010-08-19 15:44:33 +0200

    added prototype for clean fsm model specification

---
+++ examples/simple.lua
@@ -0,0 +1,8 @@
+return rfsm.csta:new {
+   on = rfsm.sista:new{},
+   off = rfsm.sista:new{},
+   
+   rfsm.trans:new{ src='off', tgt='on', events={ 'e_on' } },
+   rfsm.trans:new{ src='on', tgt='off', events={ 'e_off' } },
+   rfsm.trans:new{ src='initial', tgt='off' }
+}
