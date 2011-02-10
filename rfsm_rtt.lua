40ff7567fbd241b8e7f4014fcdb803ff664e573e
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2011-02-10 12:21:54 +0100

    add luadoc to rfsm_rtt.lua

--- rfsm_rtt.lua
+++ rfsm_rtt.lua
@@ -9,10 +9,13 @@ local assert, ipairs, pairs, type = assert, ipairs, pairs, type
 
 module("rfsm_rtt")
 
+
+--- Generate an event reader function.
 --
--- generates a function which reads all new events from the given
--- dataports and returns them in a table
+-- When called this function will read all new events from the given
+-- dataports and return them in a table.
 --
+-- @param ... list of ports to read events from
 function gen_read_events(...)
 
    local function read_events(tgttab, port)
@@ -39,8 +42,14 @@ function gen_read_events(...)
 	  end
 end
 
--- generate a function which sends the fqn out through the given
--- dataport. To be added to step_hook
+--- Generate a function which writes the fsm fqn to a port.
+--
+-- This function returns a function which takes a rfsm instance as the
+-- single parameter and write the fully qualifed state name of the
+-- active leaf to the given string rtt.OutputPort. Intended to be
+-- added to the fsm step_hook.
+--
+-- @param port rtt OutputPort to which the fqn shall be written
 function gen_write_fqn(port)
    local act_fqn = ""
    local out_dsb = rtt.Variable.new("string", string.rep(" ", 100))
@@ -57,18 +66,18 @@ function gen_write_fqn(port)
 end
 
 
--- this function launches an rfsm statemachine in the given file
+--- Lauch an rFSM statemachine in a RTT Lua Service.
+--
+-- This function launches an rfsm statemachine in the given file
 -- (specified with return csta:new{}) into a service, and optionally
--- install a eehook so that it will be periodically triggerred.
---
--- also create a port "fqn" in the TC's interface where it writes its fqn.
--- service_launch_rfsm(file, execstr_t, eehook) 
---   file: string filename
---   execstr_f: exec_string function of the resp. service
---   eehook: bool flag, if true create and enable eehook for periodic triggering.
---
--- todo: this could be done much nicer with cosmo, if we chose to add
--- that dependency.
+-- install a eehook so that it will be periodically triggerred. It
+-- also create a port "fqn" in the TC's interface where it writes the
+-- active. Todo: this could be done much nicer with cosmo, if we chose
+-- to add that dependency. 
+-- @param file file containing the rfsm model
+-- @param execstr_f exec_string function of the service. retrieve with compX:provides("Lua"):getOperation("exec_str")
+-- @param eehook boolean flag, if true eehook for periodic triggering is setup
+-- @param env table with a environment of key value pairs which will be defined in the service before anything else
 function service_launch_rfsm(file, execstr_f, eehook, env)
    local s = {}
 
