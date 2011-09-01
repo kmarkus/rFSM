f6642cf7997c8e8a012ac3ea5d805149e262eb30
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2011-09-01 08:41:21 +0200

    timeevent example: make portable

--- examples/timeevent.lua
+++ examples/timeevent.lua
@@ -1,20 +1,29 @@
 --
--- Extremly simply state machine with two states
+-- Simple timeevent demo that shows how to write portable FSM.
 --
 
 require "rfsm"
-require "rtposix"
-
 require "rfsm_timeevent"
 
-function gettime()
-   return rtposix.clock_gettime("CLOCK_MONOTONIC") 
+if rtt then
+   require "rttlib"
+   gettime = rtt.getTime
+   print("using RTT timing services")
+else
+   if pcall(require, "rtp") then
+      function gettime() return rtp.clock.gettime("CLOCK_MONOTONIC") end
+      print("using rtp timing services")
+   else
+      print("falling back on low resolution Lua time")
+      function gettime() return os.time(), 0 end
+   end
 end
 
 rfsm_timeevent.set_gettime_hook(gettime)
 
 return rfsm.csta {
    -- only for rfsm-sim
+   dbg=true,
    idle_hook=function() uml(); os.execute("sleep 0.5") end,
 
    one = rfsm.sista{},
