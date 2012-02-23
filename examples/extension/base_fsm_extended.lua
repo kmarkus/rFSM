0bcb3796d4cff1be15db887a3ec470177b74a8ab
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2012-02-23 15:24:31 +0100

    add small fsm extension example

---
+++ examples/extension/base_fsm_extended.lua
@@ -0,0 +1,16 @@
+
+local state, trans, conn = rfsm.state, rfsm.trans, rfsm.conn
+
+local fsm_model=rfsm.load("base_fsm.lua")
+
+-- add an new state
+fsm_model.emergency = state {
+   entry=function()
+	    print("performing emergency shutdown") 
+	 end,
+}
+
+fsm_model[#fsm_model+1] = trans{src='on', tgt='emergency', events={'e_emergency'} }
+fsm_model[#fsm_model+1] = trans{src='emergency', tgt='on', events={'e_emergency_recovered'} }
+
+return fsm_model
