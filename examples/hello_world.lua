b774948165bd113c86689dbb5713e8bca6f73b3c
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2012-02-28 11:27:53 +0100

    update hello_world example

--- examples/hello_world.lua
+++ examples/hello_world.lua
@@ -1,7 +1,9 @@
-return rfsm.composite_state {
-   hello = rfsm.sista{ entry=function() print("hello") end },
-   world = rfsm.simple_state{ entry=function() print("world") end },
-   rfsm.transition{ src='initial', tgt='hello' },
-   rfsm.transition{ src='hello', tgt='world', events={ 'e_done' } },
-   rfsm.transition{ src='world', tgt='hello', events={ 'e_restart' } },
+return rfsm.state {
+
+   hello = rfsm.state{ entry=function() print("hello") end },
+   world = rfsm.state{ entry=function() print("world") end },
+
+   rfsm.transition { src='initial', tgt='hello' },
+   rfsm.transition { src='hello', tgt='world', events={ 'e_done' } },
+   rfsm.transition { src='world', tgt='hello', events={ 'e_restart' } },
 }
