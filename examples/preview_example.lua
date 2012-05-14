c80a70d5cd48eb93da8c94ed8a81c94aca0dd03a
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2012-05-14 11:59:14 +0200

    add preview coordination extension

---
+++ examples/preview_example.lua
@@ -0,0 +1,43 @@
+--- Simple preview coordination example.
+-- When approaching a grasp position we consider it likely that the
+-- grasp will take place, hence it is desireable to already start
+-- positioning the arm in a convenient position. The likely predicate
+-- defines which transition is likely when (here: always the
+-- transition to 'grasping'). Grasping itself defines how "itself" can
+-- be prepared by providing a 'prepare' function, that is called when
+-- and while a transition to grasping becomes/is likely.
+
+require "rfsm_timeevent"
+require "rfsm_preview"
+local ac=require "ansicolors"
+local state, trans = rfsm.state, rfsm.trans
+
+rfsm_timeevent.set_gettime_hook(function () return os.time(), 0 end)
+
+function prnt(...) print(ac.green(ac.bright(table.concat({...}, '\t')))) end
+
+--- FSM 
+return state { 
+   idle_hook=function() uml(); os.execute("sleep 0.5") end,
+  
+   approach_grasp_pos = state {
+      entry=function () prnt("approaching grasp position") end
+   },
+
+   grasping = state {
+      prepare=function() prnt("pre-positioning arm for grasp") end,
+      entry=function() prnt("executing grasp") end
+   },
+
+   drop_off = state{
+      entry=function() prnt("dropped off object") end,
+   },
+
+   trans{ src='initial', tgt='approach_grasp_pos' },
+   trans{ src='approach_grasp_pos', tgt='grasping', 
+	  events={ 'e_after(5)' },
+	  likely=function() return true end -- static high probability of transitioning
+       },
+   trans{ src='grasping', tgt='drop_off', events={ 'e_done' } },
+   trans{ src='drop_off', tgt='approach_grasp_pos', events={ 'e_after(5)' } },
+}
---
+++ rfsm_preview.lua
@@ -0,0 +1,58 @@
+--- rFSM discrete preview coordination
+-- 
+-- This extension extends transitions with a 'likely' predicate (side
+-- effect free function) states with a 'prepare' function.. When the
+-- source state is active and the likely function returns true, then
+-- the prepare function of the target state is invoked.
+--
+
+local rfsm = require("rfsm")
+local print, ipairs, pairs, error, type = print, ipairs, pairs, error, type
+
+local actchild_get = rfsm.actchild_get
+
+module 'rfsm_preview'
+
+local function do_preview(fsm)
+   local function preview_otrs(state)
+      local likely=nil
+      local prepare=nil
+      for i,tr in ipairs(state._otrs) do
+	 likely = tr.likely
+	 if likely and likely(fsm) then
+	    prepare = tr.tgt.prepare
+	    if prepare then prepare(fsm) end
+	 end
+      end
+   end
+
+   local next = actchild_get(fsm)
+   while next do
+      preview_otrs(next)
+      next = actchild_get(next)
+   end
+end
+
+--- Setup preview coordination.
+-- @param fsm initialized root fsm.
+local function setup_preview(fsm)
+   fsm.info("rfsm_preview: discrete preview extension loaded")
+   
+   rfsm.mapfsm(function (tr)
+		  if tr.likely and type(tr.likely) ~= 'function' then
+		     error("ERROR: invalid 'preview' attribute on " .. 
+			   tostring(tr)..". Should be a function")
+		  end
+	       end, fsm, rfsm.is_trans)
+
+   rfsm.mapfsm(function (s)
+		  if s.prepare and type(s.prepare) ~= 'function' then
+		     error("ERROR: invalid 'prepare' attribute of " .. 
+			   fsm._fqn .. ". Should be a function")
+		  end
+	       end, fsm, rfsm.is_state)
+   rfsm.post_step_hook_add(fsm, do_preview)
+end
+
+-- install setup_emem as preproc hook
+rfsm.preproc[#rfsm.preproc+1] = setup_preview
